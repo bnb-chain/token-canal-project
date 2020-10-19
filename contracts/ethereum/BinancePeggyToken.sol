89459ffaf5525346b8b3b0c8feb4849619ada448
HaoyangLiu <lhy.0318@163.com>
2020-10-19 14:49:59 +0800

    fix safe transfer

--- contracts/ethereum/BinancePeggyToken.sol
+++ contracts/ethereum/BinancePeggyToken.sol
@@ -582,47 +582,6 @@ contract Claimable is Ownable {
     }
 }
 
-// File: openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol
-
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure.
- * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
-library SafeERC20 {
-    function safeTransfer(
-        ERC20Basic _token,
-        address _to,
-        uint256 _value
-    )
-    internal
-    {
-        require(_token.transfer(_to, _value));
-    }
-
-    function safeTransferFrom(
-        ERC20 _token,
-        address _from,
-        address _to,
-        uint256 _value
-    )
-    internal
-    {
-        require(_token.transferFrom(_from, _to, _value));
-    }
-
-    function safeApprove(
-        ERC20 _token,
-        address _spender,
-        uint256 _value
-    )
-    internal
-    {
-        require(_token.approve(_spender, _value));
-    }
-}
-
 // File: openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol
 
 /**
@@ -632,15 +591,13 @@ library SafeERC20 {
  * This will prevent any accidental loss of tokens.
  */
 contract CanReclaimToken is Ownable {
-    using SafeERC20 for ERC20Basic;
-
     /**
      * @dev Reclaim all ERC20Basic compatible tokens
      * @param _token ERC20Basic The address of the token contract
      */
     function reclaimToken(ERC20Basic _token) external onlyOwner {
         uint256 balance = _token.balanceOf(this);
-        _token.safeTransfer(owner, balance);
+        _token.transfer(owner, balance);
     }
 
 }
